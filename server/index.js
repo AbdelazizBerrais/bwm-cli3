524edd540b379f49520fbcf543f3d44400090efc
AbdelazizBerrais <abdelaziz.berrais@yahoo.fr>
2018-11-08 17:33:32 +0100

    Adds register, adds login and auth middleware routes on Server

--- server/index.js
+++ server/index.js
@@ -1,17 +1,28 @@
 const express = require('express');
-const config = require('./config/dev');
-const app = express();
-const Rental= require('./models/rental');
-const FakeDb=require('./models/fake-db');
-const rentalRoutes = require('./routes/rentals');
-
 const mongoose = require('mongoose');
-mongoose.connect(config.DB_URI).then(()=>{
+const bodyParser = require('body-parser');
+const config = require('./config/dev');
+
+const Rental= require('./models/rental');
+const User= require('./models/user');
+const FakeDb=require('./models/fake-db');
+
+const rentalRoutes = require('./routes/rentals'),
+      userRoutes = require('./routes/users');
+
+
+mongoose.connect(config.DB_URI,{ useNewUrlParser: true,autoIndex: false, }).then(()=>{
     const fakeDb= new FakeDb();
     fakeDb.seedDb();
 });
 
+const app=express();
+
+app.use(bodyParser.json());
+
 app.use('/api/v1/rentals',rentalRoutes);
+app.use('/api/v1/users',userRoutes);
+
 
 const PORT = process.env.PORT || 3001;
 
